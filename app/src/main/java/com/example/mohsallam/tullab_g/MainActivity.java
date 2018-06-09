affd2b0e7583c5dcad8eaad4247a39923b341f78
KhAiry <mohamedsallam953@gmail.com>
2018-06-09 16:52:05 +0200

    adding elevation ,remove main_activity.xml

--- app/src/main/java/com/example/mohsallam/tullab_g/MainActivity.java
+++ app/src/main/java/com/example/mohsallam/tullab_g/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.app_bar_navigation);
         updateResources(this,"ar");
     }
     @Override
